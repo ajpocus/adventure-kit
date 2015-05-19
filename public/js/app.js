0ce76a4d8ac95c385354b9764eb2111b48cc46cb
Austin Pocus <austin.pocus@gmail.com>
2015-05-19 19:25:28 -0400

    Working on pixel grid for drawCanvas

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import Pixel from './pixel';
+
 document.addEventListener('DOMContentLoaded', function () {
   let bgCanvas = document.getElementById('bg-canvas');
   let drawCanvas = document.getElementById('draw-canvas');
@@ -10,6 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const HEIGHT = bgCanvas.height;
   const BG_TILE_SIZE = 8;
   const PIXEL_SIZE = 32;
+
+  let PixelGrid = [];
 
   function drawBackground() {
     const NUM_TILES_HORIZ = WIDTH / BG_TILE_SIZE;
@@ -28,5 +32,46 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  function initializeDrawSurface() {
+    const NUM_PIXELS_HORIZ = WIDTH / PIXEL_SIZE;
+    const NUM_PIXELS_VERT = HEIGHT / PIXEL_SIZE;
+
+    for (let x = 0; x < NUM_PIXELS_HORIZ; x++) {
+      for (let y = 0; y < NUM_PIXELS_VERT; y++) {
+        PixelGrid.push(new Pixel(x, y));
+      }
+    }
+  }
+
+  function highlightPixel (ev) {
+    let elRect = ev.target.getBoundingClientRect();
+    let absX = ev.clientX;
+    let absY = ev.clientY;
+    let x = absX - elRect.left;
+    let y = absY - elRect.top;
+    console.log(x, y);
+
+    let pixelX = Math.floor(x / PIXEL_SIZE);
+    let pixelY = Math.floor(y / PIXEL_SIZE);
+
+    const NUM_PIXELS = (WIDTH / PIXEL_SIZE) + (HEIGHT / PIXEL_SIZE);
+
+    for (let i = 0; i < NUM_PIXELS; i++) {
+      let pixel = PixelGrid[i];
+      if (pixel.x === x && pixel.y === y) {
+        drawCtx.fillStyle = "rgba(255, 255, 255, 0.5)";
+        drawCtx.fillRect
+      }
+    }
+  }
+
+  function paintPixel (ev) {
+    let x = ev.clientX;
+    let y = ev.clientY;
+    console.log(x, y);
+  }
+
   drawBackground();
+  drawCanvas.addEventListener('mousemove', highlightPixel, false);
+  drawCanvas.addEventListener('click', paintPixel, false);
 });
