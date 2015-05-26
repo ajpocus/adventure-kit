3cd312f770aeeaf8bf71a3251b8a024ffdda1b16
Austin Pocus <austin.pocus@gmail.com>
2015-05-25 22:21:40 -0400

    Save state as framework-less app

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,9 @@
 require('babel/polyfill');
 
 let $ = require('jquery');
-let angular = require('angular');
 
 import DrawSurface from'./draw_surface';
 
 $(function () {
-  let app = angular.module('app', [require('angular-ui-router')]);
-
-  app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/draw');
-
-    $stateProvider
-      .state('draw', {
-        url: '/draw',
-        templateUrl: 'templates/draw.html'
-      })
-      .state('map', {
-        url: '/map',
-        templateUrl: 'templates/map.html'
-      })
-      .state('music', {
-        url: '/music',
-        templateUrl: 'templates/music.html'
-      });
-  });
   let drawSurface = new DrawSurface(document.getElementById('render'));
 });
