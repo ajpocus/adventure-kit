f961c835d94abe6ffcc65e69bae43417328266b6
Austin Pocus <austin.pocus@gmail.com>
2017-03-01 18:47:20 -0500

    Fix gulpfile watch directive

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task('serve', ['js', 'css'], function () {
   nodemon({
     script: 'bin/www',
     watch: ['./public'],
-    ignore: ['./public/js/all.js', './node_modules'],
+    ignore: ['./public/js/all.js', './public/dist', './node_modules'],
     tasks: ['css'],
     ext: 'css js ejs'
   });
